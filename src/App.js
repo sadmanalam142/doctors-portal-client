82184fbfe1d3bfb7b396645a897c6d89535c6c3a
sadmanalam142 <sadmanalam142@gmail.com>
2022-05-12 20:05:43 +0600

    header done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Login from './Pages/Authentication/Login/Login';
+import Register from './Pages/Authentication/Register/Register';
 import Home from './Pages/Home/Home/Home';
 import Footer from './Pages/Shared/Header/Footer';
 import Header from './Pages/Shared/Header/Header';
@@ -10,6 +13,9 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
